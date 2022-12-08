8d16b608e550c56a77bcf5bd339f911ec98a9691
wilson santana <94136600+WilsonSantanaJr@users.noreply.github.com>
2022-12-08 13:39:28 -0300

    add style.css

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- fonts play -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Play:wght@400;700&display=swap" rel="stylesheet">
     
     <link rel="stylesheet" href="css/glider.min.css">
     
